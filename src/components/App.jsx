d190945822972234947f5537a56b5f458fe01457
Prashant Bhushan Shukla <apbspbspbs@gmail.com>
2022-11-25 04:59:48 +0530

    New first Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts.js";
+import Avatar from "./Avatar";
+
+function createCard(contact){
+  return (<Card 
+    key={contact.id}
+    name= {contact.name}
+    img={contact.imgURL}    
+    phone= {contact.phone}
+    email= {contact.email}/>);
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
- <Card name= {contacts[0].name}
-    imgURL={contacts[0].imgURL}    
-    phone= {contacts[0].phone}
-    email= {contacts[0].email}/>
-    
-    <Card name= {contacts[1].name}
-    imgURL={contacts[1].imgURL}    
-    phone= {contacts[1].phone}
-    email= {contacts[1].email}/>
-    
-    <Card name= {contacts[2].name}
-    imgURL={contacts[2].imgURL}    
-    phone= {contacts[2].phone}
-    email= {contacts[2].email}/>
+      <Avatar img="https://avatars.githubusercontent.com/u/59869762?v=4"/>
+      {contacts.map(createCard)}
+
     </div>
   );
 }
